357e9a92e1e225674b71786e39509aecdbb5ed46
Diego Peres <diego_peresr13@hotmail.com>
2019-06-28 15:51:49 -0300

    Relacionamento

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=256)
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.descricao
