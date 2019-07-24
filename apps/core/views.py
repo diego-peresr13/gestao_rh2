df253aca5541086ce0cf88f826adb9b915be0ee5
Diego <diego_peresr13@hotmail.com>
2019-07-23 21:31:15 -0300

    template login

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from apps.funcionarios.models import Funcionario
 
-#@login_required
+@login_required
 def home(request):
     data = {}
     data['usuario'] = request.user
-    usuario = request.user
+    #usuario = request.user
     return render(request, 'core/index.html', data)
